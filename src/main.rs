caba6e0cef3012b9e9a094c249fccefb2d998551
AndreOGodoy <godoy.andre@hotmail.com>
2020-07-03 17:22:11 -0300

    Change rendering engine

--- src/main.rs
+++ src/main.rs
@@ -4,41 +4,51 @@ use chip8::Chip8;
 use std::error::Error;
 use std::path;
 
-use minifb::{Key, Window, WindowOptions};
+use ggez::graphics::*;
+use ggez::*;
 
-fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> ggez::GameResult<()> {
     let mut chip8 = Chip8::new();
-    
-    let path = path::Path::new("./roms/pong.rom");
-    chip8.load_program(path)?;
 
-    let mut window = Window::new("Chip-8 Simulator", 640, 320, WindowOptions::default())
-        .unwrap_or_else(|e| panic!("{}", e));
+    let path = path::Path::new("./roms/test_opcode.rom");
+    chip8.load_program(path).expect("Fail do load program");
 
-    window.limit_update_rate(Some(std::time::Duration::from_micros(66400)));
+    let cb = ContextBuilder::new("Chip8", "Andre")
+        .window_setup(ggez::conf::WindowSetup::default().title("Chip8"))
+        .window_mode(ggez::conf::WindowMode::default().dimensions(640.0, 320.0));
 
-    while window.is_open() && !window.is_key_down(Key::Escape) {
-        chip8.emulate_cycle();
+    let (ctx, event_loop) = &mut cb.build()?;
 
+    event::run(ctx, event_loop, &mut chip8)
+}
 
-        let rgb_buffer: Vec<u32> = chip8
-            .display
-            .iter()
-            .map(|bw_pixel| match bw_pixel {
-                1 => 0x00FFFFFF,
-                0 => 0x00000000,
-                _ => panic!("Display pixels must be 0 or 1"),
-            })
-            .collect();
-
-        if chip8.draw_flag {
-            window
-                .update_with_buffer(&rgb_buffer, 64, 32)
-                .unwrap_or_else(|e| panic!("Error updating: {}", e));
-        } else {
-            window.update();
-        }
+impl ggez::event::EventHandler for Chip8 {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult<()> {
+        self.emulate_cycle();
+        Ok(())
     }
 
-    Ok(())
+    fn draw(&mut self, ctx: &mut Context) -> GameResult<()> {
+        if self.draw_flag == true {
+            let mut index = 0;
+            for j in 0..32 {
+                for i in 0..64 {
+                    let rect = Rect::new(i as f32 * 10.0, j as f32 * 10.0, 10.0, 10.0);
+                    let color = match self.display[index] {
+                        0 => BLACK,
+                        1 => WHITE,
+                        _ => panic!("Deu pau na cor"),
+                    };
+
+                    Mesh::new_rectangle(ctx, DrawMode::Fill(FillOptions::default()), rect, color)
+                        .expect("Deu pau na mesh")
+                        .draw(ctx, DrawParam::default())
+                        .expect("Deu pau no draw");
+
+                    index += 1;
+                }
+            }
+        }
+        present(ctx)
+    }
 }
